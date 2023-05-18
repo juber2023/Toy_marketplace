5af36b8a108a37f10a4d7f4a47ad3f686d60b10c
juber2023 <juber.eco@gmail.com>
2023-05-18 13:34:02 +0600

    err page added

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            not found
+        <div className='flex justify-center items-center flex-col h-screen'>
+            <div >
+                <img className='h-96' src="https://img.freepik.com/free-vector/404-error-with-tired-person-concept-illustration_114360-7869.jpg?w=826&t=st=1684394373~exp=1684394973~hmac=8e9af6b1ccd2be0fe4a7147f454f831af9486ef3f5535fd10c995d8f7e2c8877" alt="" />
+            </div>
+            <button className='button'>
+                <Link to='/'>Back to home</Link>
+            </button>
         </div>
     );
 };
